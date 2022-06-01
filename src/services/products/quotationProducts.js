97120f216d510dd19e2ebf2bff7036f51c49b4d5
Bryan Henríquez <Bryan280801>
2022-06-01 00:40:19 -0500

    feat: shop data in quotation products

--- src/services/products/quotationProducts.js
+++ src/services/products/quotationProducts.js
@@ -23,7 +23,7 @@ const getQuotationProducts = (quotation_id) => {
   return new Promise(async (resolve, reject) => {
     try {
       const products = await sequelize.query(
-        `SELECT product.id, shop_has_product.id AS "shopProductId", product.name, shop_has_product.price, quotation_has_product."createdAt" FROM quotation_has_product JOIN shop_has_product ON shop_has_product.id = quotation_has_product.shop_has_product_id JOIN product ON product.id = shop_has_product.product_id WHERE quotation_has_product.quotation_id = ${quotation_id}`,
+        `SELECT product.id, shop_has_product.id AS "shopProductId", product.name, shop.name AS shop, shop.id AS "shopId", shop_has_product.price, quotation_has_product."createdAt" FROM quotation_has_product JOIN shop_has_product ON shop_has_product.id = quotation_has_product.shop_has_product_id JOIN product ON product.id = shop_has_product.product_id JOIN shop ON shop_has_product.shop_id = shop.id WHERE quotation_has_product.quotation_id = ${quotation_id}`,
         { type: sequelize.QueryTypes.SELECT }
       );
 
